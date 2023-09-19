9e446690f8a5c1cc8e317b28b538426608ffa423
Tallal Khan <Pc0710tallal@outlook.com>
2023-09-19 02:31:56 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import CurrentWeather from './components/CurrentWeather';
+import ForecastWeather from './components/ForecastWeather';
+import "./index.css"
+import { Search } from 'react-bootstrap-icons';
+
+function App() {
+  const [searchValue, setsearchValue] = useState("")
+  const [weatherData, setWeatherData] = useState("")
+  const [error, setError] = useState("")
+
+
+
+  const Calling_weather = async (country_city) => {
+    const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '74c54d1055msh80666a5280e465bp14ca73jsnfffe828b5822',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(`${url}q=${country_city}&days=3`, options);
+      const data = await response.json();
+
+      if (data.error) {
+        setError("Location not found or it is not listed in the API"); // Set error message
+        setWeatherData(""); // Clear weather data
+      }
+
+      else {
+        setError(""); // Clear error message
+        setWeatherData(data); // Set weather data
+      }
+    }
+
+    catch (error) {
+      console.error(error);
+      setError("An error occurred"); // Set error message
+      setWeatherData(""); // Clear weather data
+    }
+  }
+
+
+
+  useEffect(() => {
+    Calling_weather('karachi')
+  }, [])
+
+
+
+  return (
+    <div className='min-h-screen px-14 py-10 font-poppins bg-gradient-to-r from-cyan-200 to-blue-400'>
+      <div className=' max-w-screen-md mx-auto '>
+        <div className='w-full relative'>
+          <input
+            className='outline-none w-full px-4 py-2 rounded-xl bg-gray-100 text-gray-800 text-sm'
+            type="text"
+            placeholder='Enter a country/city'
+            value={searchValue}
+            onChange={(e) => setsearchValue(e.target.value)}
+          />
+
+          <button
+            className='absolute top-3 right-5 scale-110'
+            onClick={() => Calling_weather(searchValue)}
+          >
+            <Search />
+          </button>
+        </div>
+
+        {error && <div className='text-red-500'>{error}</div>} {/* Display error message */}
+        <CurrentWeather weatherData={weatherData} />
+        <ForecastWeather weatherData={weatherData} />
+      </div>
+    </div>
+
+  )
+}
+export default App;
---
+++ src/components/CurrentWeather.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Wind, DropletFill, Hurricane, BodyText, CloudFill } from 'react-bootstrap-icons';
+
+
+const CurrentWeather = ({ weatherData }) => {
+
+    const RenderInfo = () => {
+        if (!weatherData) {
+            console.log('Loading weather data...')
+            return <div>Loading weather data...</div>;
+        }
+
+        const { location, current } = weatherData;
+        const { name, region, country } = location;
+        const { temp_c, wind_kph, humidity, feelslike_c, pressure_mb, condition } = current;
+        const { icon, text } = condition;
+
+
+        return (
+            <div className='font-montserrat flex justify-between w-full mt-6 rounded-xl p-4 bg-white bg-opacity-80 backdrop-blur-md'>
+                {/* right_side */}
+                <div className='flex-1'>
+                    <div className='font-semibold text-xl text-gray-800'>{name}</div>
+                    <div className='-my-2 text-gray-800'>{country}</div>
+                    <div className='text-gray-800'>{region}</div>
+
+                    <div className='flex justify-around items-center gap-x-7 my-10'>
+                        <div className='flex justify-between '>
+                            <img src={icon} alt="img" className='scale-[1.8]' />
+                        </div>
+                        <div className='font-light text-7xl'>{temp_c}&deg;</div>
+                    </div>
+                    <div className='ml-2 -mt-5 font-semibold text-slate-500'>
+                        {text}
+                    </div>
+                </div>
+
+
+                {/* left_side */}
+                <div className='flex flex-col justify-center text-start capitalize flex-1 ml-14'>
+
+                    <p className='flex items-center gap-x-5'>
+                        <BodyText />
+                        <span>feels like  </span>{feelslike_c} &deg;
+                    </p>
+
+                    <p className='flex items-center gap-x-5'>
+                        <DropletFill />
+                        <span>humidity  </span> {humidity}%
+                    </p>
+
+                    <p className='flex items-center gap-x-5'>
+                        <Wind />
+                        <span className='mr-7'>wind  </span>{wind_kph}kph
+                    </p>
+
+                    <p className='flex items-center gap-x-5'>
+                        <Hurricane />
+                        <span>pressure  </span>{pressure_mb}mb
+                    </p>
+
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            {RenderInfo()}
+        </div>
+    )
+}
+
+export default CurrentWeather
+
+
+
+
